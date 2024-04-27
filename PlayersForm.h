4a90fd5f046b6ef6372b9f53141db3d072770699
caraxesmsc <mohamedongo21@gmail.com>
2024-04-28 00:49:53 +0300

    changed to my conentcion

--- PlayersForm.h
+++ PlayersForm.h
@@ -16,7 +16,7 @@ namespace FootballAssociationDB {
 	public ref class PlayersForm : public System::Windows::Forms::Form
 	{
 	public:
-		String^ connectionStr = "Data Source=KassabLaptop;Initial Catalog=Football_Association;Integrated Security=True;"; // da el connection string 3ashan a3ml connect m3 el database W LAZM T5'YARO L BTA3K ENTA
+		String^ connectionStr = "Data Source=LAPTOP-ABIJFMBV;Initial Catalog=Football_AssociationFinal;Integrated Security=True;"; // da el connection string 3ashan a3ml connect m3 el database W LAZM T5'YARO L BTA3K ENTA
 
 		PlayersForm(void)
 		{
